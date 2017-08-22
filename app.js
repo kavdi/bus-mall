fd80d2a3e80cb430c4b38cb4690486e8d6ae7a49
kavdihodgson <kavdyjh@gmail.com>
2017-08-22 13:47:05 -0700

    got pictures to show up

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 'use strict';
 var display = [];
 var options = [];
-var recycle = [];
-function Pics (name, url, timesShown, timesClicked){
+// var recycle = [];
+
+function Pics (name, source, timesShown, timesClicked){
   this.name = name;
-  this.url = url;
-  this.timesShown = timesShown;
-  this.timesClicked = timesClicked;
+  this.source = source;
+  this.id = name;
+  this.timesShown = 0;
+  this.timesClicked = 0;
   options.push(this);
 
 }
@@ -32,40 +34,56 @@ new Pics('usb', 'assets/usb.gif', 0, 0);
 new Pics('waterCan', 'assets/water-can.jpg', 0, 0);
 new Pics('wineGlass', 'assets/wine-glass.jpg', 0, 0);
 
-var showPictures = function(){
-  var getTable = document.getElementById('pictures');
-  var getRow = document.createElement('tr');
-  var setPicture = document.createElement('td');
-  setPicture.ClassName = 'options';
-  setPicture.innerText = placeholder;
-  getRow.appendChild(setPicture);
-  getTable.appendChild(getRow);
+var showPic = function(name, source, id){
+  var table = document.getElementById('pictures');
+  var data = document.createElement('td');
+  table.appendChild(data);
+  var pic = document.createElement('img');
+  img.ClassName = 'images';
+  pic.src = placeholder.source;
+  data.appendChild(pic);
 };
+// var showPictures = function(source, picName, id){
+//   var getTable = document.getElementById('pictures');
+//   var getRow = document.createElement('tr');
+//   getTable.appendChild(getRow);
+//   var setPicture = document.createElement('td');
+//   setPicture.ClassName = 'img';
+//   getRow.appendChild(setPicture);
+//   var insert = document.createElement('img');
+//   element.setAttribute('src', source);
+//   element.setAttribute('class', picName);
+//   element.setAttribute('name', id);
+//   setPicture.appendChild(insert);
+// };
 
 for (var i = 0; i < 3; i++){
   var rndmPic = Math.floor(Math.random() * options.length);
   var placeholder = options[rndmPic];
   display.push(placeholder);
-  options.splice(rndmPic, 1);
-  showPictures();
-};
+  showPic();
 
+
+  // options.splice(rndmPic, 1);
+  // showPictures(Pics.source, 'img', Pics.name);
+};
 console.log(display);
 
-var newOptions = function(){
-  for (var x = 0; x < 3; x++){
-    recycle.push(display[0]);
-    console.log(recycle);
-    display = [];
-    console.log(display);
-    for (var i = 0; i < 3; i++){
-      var rndmPic = Math.floor(Math.random() * options.length);
-      var placeholder = options[rndmPic];
-      display.push(placeholder);
-      options.splice(rndmPic, 1);
-      options.push(recycle);
-    }
-  }
-};
-newOptions();
-console.log(options);
+
+// var newOptions = function(){
+//   for (var x = 0; x < 3; x++){
+//     recycle.push(display[0]);
+//     console.log(recycle);
+//     display = [];
+//     console.log(display);
+//     for (var i = 0; i < 3; i++){
+//       var rndmPic = Math.floor(Math.random() * options.length);
+//       var placeholder = options[rndmPic];
+//       display.push(placeholder);
+//       options.splice(rndmPic, 1);
+//       options.push(recycle);
+//     }
+//   }
+// };
+// newOptions();
+// console.log(options);
