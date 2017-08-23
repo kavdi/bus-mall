8bddc4c238054140041fda91d485ea41878ab465
kavdihodgson <kavdyjh@gmail.com>
2017-08-23 00:09:50 -0700

    stuck on click

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var showPic = function(name, source, id){
   var data = document.createElement('td');
   table.appendChild(data);
   var pic = document.createElement('img');
-  pic.id = "clickMe";
+  pic.setAttribute('id', 'clickMe');
   pic.src = placeholder.source;
   data.appendChild(pic);
 };
@@ -49,11 +49,10 @@ for (var i = 0; i < 3; i++){
   var rndmPic = Math.floor(Math.random() * options.length);
   var placeholder = options[rndmPic];
   display.push(placeholder);
-  placeholder[i].timesShown += 1;
+  display[i].timesShown += 1;
   showPic(display[i].name, display[i].source, display[i].name);
 }
-// options.splice(rndmPic,1);
-// options.push(display);
+console.log(display);
 
 var cyclePics = function(){
   display = [];
@@ -64,13 +63,13 @@ var cyclePics = function(){
       var rndmPic = Math.floor(Math.random() * options.length);
     } else {
       display.push(placeholder);
-      placeholder[i].timesShown += 1;
+      display[i].timesShown += 1;
       showPic(display[i].name, display[i].source, display[i].name);
     }
   }
 };
 
-var click = document.getElementById('clickMe');
+var click = document.getElementsByTagName('img');
 click.addEventListener('click', chosen);
 
 var numberClicked = 0;
@@ -82,7 +81,15 @@ function chosen(event) {
       numberClicked++;
     } else if (numberClicked === number) {
       click.removeEventListener('click', chosen);
-
+      var counter = document.getElementById('dataResult');
+      var listVotes = document.createElement('ul');
+      counter.appendChild(listVotes);
+      for (var c = 0; c < options.length; c++) {
+        var showData = document.createElement('li');
+        showData.innerText = 'You chose ' + options[c].name + options[c].timesClicked + ' times.';
+        listVotes.appendChild(showData);
+      }
+      break;
     }
   }
 }
@@ -90,8 +97,8 @@ function chosen(event) {
 // var canvas = document.getElementById('canvas');
 // var ctx = canvas.getContext('2d');
 
-// var scatterChart = new Chart(ctx, {
-//     type: 'scatter',
+// var barChart = new Chart(ctx, {
+//     type: 'bar',
 //     data: {
 //         datasets: [{
 //             label: 'Scatter Dataset',
@@ -116,42 +123,42 @@ function chosen(event) {
 //         }
 //     }
 // });
-
+//
 // var chartConfig = {
 //   type: 'bar',
 //   data: {
 //     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'], // x-axis labels for every entry in your data set. It should match up with the number of things you're plotting (if it's a bar chart)
-    // datasets: [{ // <-- notice that this can be an array of multiple data sets.
-      // each data set is its own object literal.
-      // label: '# of Votes', // <-- the label of this one data set
-      // data: [12, 19, 3, 5, 2, 3], // <-- where your data actually goes. just the numbers
-      // backgroundColor: [ // <-- this can be either one single color or a color for each item in your bar chart.
-  //       'rgba(255, 99, 132, 0.2)',
-  //       'rgba(54, 162, 235, 0.2)',
-  //       'rgba(255, 206, 86, 0.2)',
-  //       'rgba(75, 192, 192, 0.2)',
-  //       'rgba(153, 102, 255, 0.2)',
-  //       'rgba(255, 159, 64, 0.2)'
-  //     ],
-  //     borderColor: [
-  //       'rgba(255,99,132,1)',
-  //       'rgba(54, 162, 235, 1)',
-  //       'rgba(255, 206, 86, 1)',
-  //       'rgba(75, 192, 192, 1)',
-  //       'rgba(153, 102, 255, 1)',
-  //       'rgba(255, 159, 64, 1)'
-  //     ],
-  //     borderWidth: 5 // border width in pixels
-  //   }]
-  // },
-  // options: {
-    // maintainAspectRatio: false,
-    // animation: {
-    //   duration: 1000
-    // },
+//     datasets: [{ // <-- notice that this can be an array of multiple data sets.
+//       each data set is its own object literal.
+//       label: '# of Votes', // <-- the label of this one data set
+//       data: [12, 19, 3, 5, 2, 3], // <-- where your data actually goes. just the numbers
+//       backgroundColor: [ // <-- this can be either one single color or a color for each item in your bar chart.
+//         'rgba(255, 99, 132, 0.2)',
+//         'rgba(54, 162, 235, 0.2)',
+//         'rgba(255, 206, 86, 0.2)',
+//         'rgba(75, 192, 192, 0.2)',
+//         'rgba(153, 102, 255, 0.2)',
+//         'rgba(255, 159, 64, 0.2)'
+//       ],
+//       borderColor: [
+//         'rgba(255,99,132,1)',
+//         'rgba(54, 162, 235, 1)',
+//         'rgba(255, 206, 86, 1)',
+//         'rgba(75, 192, 192, 1)',
+//         'rgba(153, 102, 255, 1)',
+//         'rgba(255, 159, 64, 1)'
+//       ],
+//       borderWidth: 5 // border width in pixels
+//     }]
+//   },
+//   options: {
+//     maintainAspectRatio: false,
+//     animation: {
+//       duration: 1000
+//     },
 //     title: {
 //       display: true,
-//       text: 'Some stuff and some junk'
+//       text:
 //     },
 //     scales: {
 //       yAxes: [{
