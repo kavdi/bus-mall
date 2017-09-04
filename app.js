654803f24e518da8f082ee105fed527449b9c2d0
kavdihodgson <kavdyjh@gmail.com>
2017-09-04 11:41:15 -0700

    fixed all errors with js and styled a bit in css

--- app.js
+++ app.js
@@ -1,67 +1,71 @@
 'use strict';
-var display = [];
-var options = [];
 
-function Pics (name, source, timesShown, timesClicked){
+var totalClicks = 0;
+var maxClicks = 25;
+
+function Item (name, source, id) {
   this.name = name;
   this.source = source;
-  this.id = name;
   this.timesShown = 0;
   this.timesClicked = 0;
-  options.push(this);
-
+  this.id = id;
+  itemsList.push(this);
 }
+var itemsList = [];
 
-new Pics('bag', 'assets/bag.jpg', 0, 0);
-new Pics('banana', 'assets/banana.jpg', 0, 0);
-new Pics('bathroom', 'assets/bathroom.jpg', 0, 0);
-new Pics('boots', 'assets/boots.jpg', 0, 0);
-new Pics('breakfast', 'assets/breakfast.jpg', 0, 0);
-new Pics('bubblegum', 'assets/bubblegum.jpg', 0, 0);
-new Pics('chair', 'assets/chair.jpg', 0, 0);
-new Pics('cthulhu', 'assets/cthulhu.jpg', 0, 0);
-new Pics('dogDuck', 'assets/dog-duck.jpg', 0, 0);
-new Pics('dragon', 'assets/dragon.jpg', 0, 0);
-new Pics('pen', 'assets/pen.jpg', 0, 0);
-new Pics('petSweep', 'assets/pet-sweep.jpg', 0, 0);
-new Pics('scissors', 'assets/scissors.jpg', 0, 0);
-new Pics('shark', 'assets/shark.jpg', 0, 0);
-new Pics('sweep', 'assets/sweep.png', 0, 0);
-new Pics('tauntaun', 'assets/tauntaun.jpg', 0, 0);
-new Pics('unicorn', 'assets/unicorn.jpg', 0, 0);
-new Pics('usb', 'assets/usb.gif', 0, 0);
-new Pics('waterCan', 'assets/water-can.jpg', 0, 0);
-new Pics('wineGlass', 'assets/wine-glass.jpg', 0, 0);
+var names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var paths = ['assets/bag.jpg', 'assets/banana.jpg', 'assets/bathroom.jpg', 'assets/boots.jpg', 'assets/breakfast.jpg', 'assets/bubblegum.jpg', 'assets/chair.jpg', 'assets/cthulhu.jpg', 'assets/dog-duck.jpg', 'assets/dragon.jpg', 'assets/pen.jpg', 'assets/pet-sweep.jpg', 'assets/scissors.jpg', 'assets/shark.jpg', 'assets/sweep.png', 'assets/tauntaun.jpg', 'assets/unicorn.jpg', 'assets/usb.gif', 'assets/water-can.jpg', 'assets/wine-glass.jpg'];
+var ids = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 
-var showPic = function(name, source, id){
-  var table = document.getElementById('pictures');
-  var data = document.createElement('td');
-  table.appendChild(data);
-  var pic = document.createElement('img');
-  pic.src = placeholder.source;
-  data.appendChild(pic);
-};
-
-for (var i = 0; i < 3; i++){
-  var rndmPic = Math.floor(Math.random() * options.length);
-  var placeholder = options[rndmPic];
-  display.push(placeholder);
-  showPic(display[i].name, display[i].source, display[i].name);
+function createItems (){
+  for (var i = 0; i < names.length; i++){
+    new Item(names[i], paths[i], ids[i]);
+  }
 }
-// options.splice(rndmPic,1);
-// options.push(display);
+createItems();
 
-var cyclePics = function(){
-  for (var i = 0; i < 3; i++){
-    var rndmPic = Math.floor(Math.random() * options.length);
-    var placeholder = options[rndmPic];
-    if (placeholder.includes(display[i])){
-      var rndmPic = Math.floor(Math.random() * options.length);
+var thisRound = [];
+var lastRound = [];
+
+function makeThreeImages (){
+  for (var i = 1; i < 4; i++) {
+    var indexNum = Math.floor(Math.random() * itemsList.length);
+    if (lastRound.includes(indexNum) || thisRound.includes(indexNum)) {
+      i--;
     } else {
-      display = [];
-      display.push(placeholder);
-      showPic();
+      thisRound.push(indexNum);
+      itemsList[indexNum].timesShown++;
+      var linkedImage = document.getElementById('image-' + i);
+      linkedImage.setAttribute('src', itemsList[indexNum].source);
+      linkedImage.setAttribute('itemIdx', indexNum);
     }
   }
-};
-console.log(display);
+  lastRound = thisRound;
+  thisRound = [];
+}
+makeThreeImages();
+
+for (var i = 0; i < document.getElementsByClassName('clickable').length; i++) {
+  var image = document.getElementById('image-' + (i + 1));
+  image.addEventListener('click', onClick);
+}
+
+function onClick (event){
+  var itemIdx = parseInt(event.target.getAttribute('itemIdx'));
+  var itemIWant = itemsList[itemIdx];
+  itemIWant.timesClicked++;
+  makeThreeImages();
+  totalClicks++;
+  if (totalClicks === maxClicks) {
+    for (var i = 0; i < document.getElementsByClassName('clickable').length; i++) {
+      var image = document.getElementById('image-' + (i + 1));
+      image.removeEventListener('click', onClick);
+    }
+    var list = document.getElementById('list');
+    for (var j = 0; j < itemsList.length; j++) {
+      var li = document.createElement('li');
+      li.innerText = itemsList[j].name + ' was clicked ' + itemsList[j].timesClicked + ' times';
+      list.appendChild(li);
+    }
+  }
+}
